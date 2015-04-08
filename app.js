9d3cbb2b70de9e5fe4042da49f2ad9f3c7a8dae6
Mike Swartz <mswartz@gmail.com>
2015-04-08 16:43:57 -0400

    transition prototyping

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 $(document).ready( function() {
-	$('.img-node').on('click', function(){
-		// console.log('works');
-		$(this).toggleClass('open');
+	// $('.img-node').on('click', function(){
+	// 	// console.log('works');
+	// 	$(this).toggleClass('open');
+	// });
+
+	//when button is clicked, original image zooms/fades out and new image fades in
+	$('#button-1').on('click', function(){
+		$('#slide-1').toggleClass('active');
+		$('#slide-2').toggleClass('active');
 	});
 
-//when button is clicked, original image zooms/fades out and new image fades in
-	$('.button1').on('click', function(){
-		$('.fadeOut').toggle('slow');
-		$('.fadeIn').css('display', 'block')
+	$('#button-4').on('click', function(){
+		$('#slide-2').toggleClass('active');
+		$('#slide-1').toggleClass('active');
 	});
 
 });
