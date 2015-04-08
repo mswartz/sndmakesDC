59d582966db546aa669ed2503327f671ad37d469
Ashley <ashleywu2016@u.northwestern.edu>
2015-04-08 16:29:21 -0400

    some more animation

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ $(document).ready( function() {
 		// console.log('works');
 		$(this).toggleClass('open');
 	});
+
+//when button is clicked, original image zooms/fades out and new image fades in
+	$('.button1').on('click', function(){
+		$('.fadeOut').toggle('slow');
+		$('.fadeIn').css('display', 'block')
+	});
+
 });
