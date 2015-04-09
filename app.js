0600106a366d60087b825f4dc65ef8df851c670f
Mike Swartz <mswartz@gmail.com>
2015-04-09 10:48:32 -0400

    animation prototype done

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 $(document).ready( function() {
-	// $('.img-node').on('click', function(){
-	// 	// console.log('works');
-	// 	$(this).toggleClass('open');
-	// });
 
 	//when button is clicked, original image zooms/fades out and new image fades in
 	$('#button-1').on('click', function(){
-		$('#slide-1').toggleClass('active');
-		$('#slide-2').toggleClass('active');
+		$('#slide-2').removeClass('hidden').addClass('zoom-in');
+		$('#slide-1').addClass('hidden').removeClass('zoom-in');
 	});
 
+	//toggle back to slide 1
 	$('#button-4').on('click', function(){
-		$('#slide-2').toggleClass('active');
-		$('#slide-1').toggleClass('active');
+		$('#slide-1').removeClass('hidden').addClass('zoom-in');
+		$('#slide-2').addClass('hidden').removeClass('zoom-in');
 	});
 
 });
