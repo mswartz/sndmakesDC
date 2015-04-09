78a43761fbeb93b05c8a86222401f039d78bde34
Welch Canavan <welch.canavan@gmail.com>
2015-04-09 15:30:08 -0400

    add hash functionality

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ $.getJSON( '/images.json', function( data ){
 	var template = Handlebars.compile( source );
 	$( '.container' ).html( template( images ) );
 
-	console.log( data );
+	if( window.location.hash ) {
+		var hash = window.location.hash.substring(1);
+
+		$( '.container .media-container[data-id="1"]').addClass('hidden').removeClass('zoom-in')
+		$( '.container .media-container[data-id="' + hash + '"]').removeClass('hidden').addClass('zoom-in');
+	} else {
+		console.log( "no hash!" );
+	}
 });
 
+// Switch images
 
 $( '.container' ).on( 'click', '.media-button', function() {
 	var _this = this;
@@ -17,8 +25,18 @@ $( '.container' ).on( 'click', '.media-button', function() {
 	var oldImage = $( '.media-container[data-id="' + oldID + '"]' );
 	var newImage = $( '.media-container[data-id="' + newID + '"]' );
 
+	window.location.hash = '#' + newID;
+
 	$( oldImage ).addClass('hidden').removeClass('zoom-in');
 	$( newImage ).removeClass('hidden').addClass('zoom-in');
+});
+
+
+// Back to the beginning
+
+$( '.logo-trigger' ).on( 'click', function() {
+	$( '.container .media-container.zoom-in').addClass('hidden').removeClass('zoom-in');
+	$( '.container .media-container[data-id="1"]').removeClass('hidden').addClass('zoom-in')
 });
 
 
